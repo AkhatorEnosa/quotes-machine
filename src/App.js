b054842cf20adabd3a84bf654630d71a3d72cdc0
AkhatorEnosa <aosakhogba@gmail.com>
2021-09-24 08:27:47 +0100

    all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      quotes: []
+    }
+  }
+ 
+    componentDidMount() {
+        fetch('https://type.fit/api/quotes')
+        .then(response => response.json())
+        .then(data => this.setState({ quotes: data}))
+    }
+
+    newQuote = () => {
+      this.componentDidMount();
+    }
+  
+  render() {
+    var quotes = this.state.quotes;
+    var randomNum = Math.floor(Math.random() * quotes.length);
+    
+    if (this.state.quotes.length === 0) {
+      return (
+        <div id="quote-box">
+          <p class="loading">Loading...</p>
+        </div>
+      )
+    } else {
+      var quoteText = quotes[randomNum].text;
+      var quoteAuthor = "- "+quotes[randomNum].author;
+
+      return (
+        <div id="quote-box">
+          <p id="text"><span class="quote-mark">"</span>{quoteText}</p>
+          <p id="author">{quoteAuthor}</p>
+          <div id="link-and-btn">
+            <a id="tweet-quote" href={"https://twitter.com/intent/tweet?text=" + quoteText + quoteAuthor} target="_blank" rel="noreferrer">Tweet</a>
+            <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+          </div>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
